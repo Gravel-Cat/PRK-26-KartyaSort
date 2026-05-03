a87d2d40c670e4135b5b0084dda7f233873970f3
Juhász Benedek <juhaszbeni51@gmail.com>
2026-05-03 16:48:43 +0200

    Kártya structure reworked, fixed issues with missing getters and constructors

--- src/main/java/hu/nye/progkorny/model/Kartya.java
+++ src/main/java/hu/nye/progkorny/model/Kartya.java
@@ -6,4 +6,35 @@ import lombok.Data;
 public class Kartya {
     private final Long id;
     private final String name;
+    private final String expansion;
+    private final String type;
+    private final String rarity;
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getExpansion() {
+        return expansion;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public String getRarity() {
+        return rarity;
+    }
+
+    public Kartya(Long id, String name, String expansion, String type, String rarity) {
+        this.id = id;
+        this.name = name;
+        this.expansion = expansion;
+        this.type = type;
+        this.rarity = rarity;
+    }
 }
