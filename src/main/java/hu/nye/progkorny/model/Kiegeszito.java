fd5b34e154da76ef807e815bfd720efec8a425dd
Juhász Benedek <juhaszbeni51@gmail.com>
2026-05-11 17:22:04 +0200

    Added base class for Kiegeszito, linked with Kartya

---
+++ src/main/java/hu/nye/progkorny/model/Kiegeszito.java
@@ -0,0 +1,36 @@
+package hu.nye.progkorny.model;
+
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+public class Kiegeszito {
+    private final Long id;
+    private final String name;
+    private final int size;
+    private final List<Kartya> cards;
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public List<Kartya> getCards() {
+        return cards;
+    }
+
+    public Kiegeszito(Long id, String name, int size, List<Kartya> cards) {
+        this.id = id;
+        this.name = name;
+        this.size = size;
+        this.cards = cards;
+    }
+}
