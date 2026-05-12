7903a6c309886db4819edcf5799f6e7365f856b7
Juhász Benedek <juhaszbeni51@gmail.com>
2026-05-12 16:24:15 +0200

    Reformatted code to use static methods

--- src/main/java/hu/nye/progkorny/repository/KartyaRepository.java
+++ src/main/java/hu/nye/progkorny/repository/KartyaRepository.java
@@ -9,39 +9,39 @@ import org.springframework.web.bind.annotation.PathVariable;
 
 @Repository
 public class KartyaRepository {
-    Kartya kartya1 = new Kartya(1L, "Leafeon", "grass", "sinnoh", "rare");
-    Kartya kartya2 = new Kartya(2L, "Vivillon", "grass", "kalos", "common");
-    Kartya kartya3 = new Kartya(3L, "Rotom", "electric", "sinnoh", "common");
-    Kartya kartya4 = new Kartya(4L, "Flygon", "dragon", "hoenn", "uncommon");
-    Kartya kartya5 = new Kartya(5L, "Dubwool", "normal", "galar", "rare");
-    Kartya kartya6 = new Kartya(6L, "Ceruledge", "fire", "paldea", "uncommon");
-    Kartya kartya7 = new Kartya(7L, "Meganium", "grass", "johto", "uncommon");
-    Kartya kartya8 = new Kartya(8L, "Glimmora", "fighting", "paldea", "common");
-    Kartya kartya9 = new Kartya(9L, "Chandelure", "psychic", "kalos", "common");
-    Kartya kartya10 = new Kartya(10L, "Lucario", "fighting", "sinnoh", "rare");
-    List<Kartya> kartyak = List.of(kartya1, kartya2, kartya3, kartya4, kartya5, kartya6, kartya7, kartya8, kartya9, kartya10);
+    static Kartya kartya1 = new Kartya(1L, "Leafeon", "grass", "sinnoh", "rare");
+    static Kartya kartya2 = new Kartya(2L, "Vivillon", "grass", "kalos", "common");
+    static Kartya kartya3 = new Kartya(3L, "Rotom", "electric", "sinnoh", "common");
+    static Kartya kartya4 = new Kartya(4L, "Flygon", "dragon", "hoenn", "uncommon");
+    static Kartya kartya5 = new Kartya(5L, "Dubwool", "normal", "galar", "rare");
+    static Kartya kartya6 = new Kartya(6L, "Ceruledge", "fire", "paldea", "uncommon");
+    static Kartya kartya7 = new Kartya(7L, "Meganium", "grass", "johto", "uncommon");
+    static Kartya kartya8 = new Kartya(8L, "Glimmora", "fighting", "paldea", "common");
+    static Kartya kartya9 = new Kartya(9L, "Chandelure", "psychic", "kalos", "common");
+    static Kartya kartya10 = new Kartya(10L, "Lucario", "fighting", "sinnoh", "rare");
+    static List<Kartya> kartyak = List.of(kartya1, kartya2, kartya3, kartya4, kartya5, kartya6, kartya7, kartya8, kartya9, kartya10);
 
-    public List<Kartya> getAllKartya() {
+    public static List<Kartya> getAllKartya() {
         return kartyak;
     }
 
-    public Kartya getKartyaById(@PathVariable Long id) {
+    public static Kartya getKartyaById(@PathVariable Long id) {
         return kartyak.stream().filter(kartya -> kartya.getId().equals(id)).findFirst().get();
     }
 
-    public List<Kartya> getKartyaByName (@PathVariable String name) {
+    public static List<Kartya> getKartyaByName (@PathVariable String name) {
         return kartyak.stream().filter(kartya -> kartya.getName().equals(name)).collect(Collectors.toList());
     }
 
-    public List<Kartya> getKartyaByType (@PathVariable String type) {
+    public static List<Kartya> getKartyaByType (@PathVariable String type) {
         return kartyak.stream().filter(kartya -> kartya.getType().equals(type)).collect(Collectors.toList());
     }
 
-    public List<Kartya> getKartyaByExpansion(@PathVariable String expansion) {
+    public static List<Kartya> getKartyaByExpansion(@PathVariable String expansion) {
         return kartyak.stream().filter(kartya -> kartya.getExpansion().equals(expansion)).collect(Collectors.toList());
     }
 
-    public List<Kartya> getKartyaByRarity (@PathVariable String rarity) {
+    public static List<Kartya> getKartyaByRarity (@PathVariable String rarity) {
         return kartyak.stream().filter(kartya -> kartya.getRarity().equals(rarity)).collect(Collectors.toList());
     }
 }
--- src/main/java/hu/nye/progkorny/service/KartyaService.java
+++ src/main/java/hu/nye/progkorny/service/KartyaService.java
@@ -4,35 +4,31 @@ import java.util.List;
 
 import hu.nye.progkorny.model.Kartya;
 import hu.nye.progkorny.repository.KartyaRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class KartyaService {
-    @Autowired
-    KartyaRepository kartyaRepository;
-
     public List<Kartya> getAllKartya() {
-        return kartyaRepository.getAllKartya();
+        return KartyaRepository.getAllKartya();
     }
 
     public Kartya getKartyaById(Long id) {
-        return kartyaRepository.getKartyaById(id);
+        return KartyaRepository.getKartyaById(id);
     }
 
     public List<Kartya> getKartyaByName(String name) {
-        return kartyaRepository.getKartyaByName(name);
+        return KartyaRepository.getKartyaByName(name);
     }
 
     public List<Kartya> getKartyaByType(String type) {
-        return kartyaRepository.getKartyaByType(type);
+        return KartyaRepository.getKartyaByType(type);
     }
 
     public List<Kartya> getKartyaByExpansion(String expansion) {
-        return kartyaRepository.getKartyaByExpansion(expansion);
+        return KartyaRepository.getKartyaByExpansion(expansion);
     }
 
     public List<Kartya> getKartyaByRarity(String rarity) {
-        return kartyaRepository.getKartyaByRarity(rarity);
+        return KartyaRepository.getKartyaByRarity(rarity);
     }
 }
